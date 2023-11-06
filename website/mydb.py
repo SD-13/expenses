695fd082df19a51d4485d8a7b38a9d9701f37083
Sujay <sujaydey641@gmail.com>
2023-11-06 23:46:04 +0530

    initial work

---
+++ website/mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password'
+)
+
+# prepare a cursor object
+cursorObject = database.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE pirate")
+
+print("ALL Done!")
